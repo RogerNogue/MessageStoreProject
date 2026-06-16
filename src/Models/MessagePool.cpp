024b78cde26eb1c52420e8d4d010bad65931354e
RogerNogue <rogernogueb@gmail.com>
2026-06-16 17:09:27 +0200

    refactor: const reference on argument that could use it

--- src/Models/MessagePool.cpp
+++ src/Models/MessagePool.cpp
@@ -11,7 +11,7 @@ void MessagePool::StoreMessage(Message message)
 	messages[message.GetDestination().ID].emplace_back(std::move(message));
 }
 
-std::deque<Message> MessagePool::ReadMessages(User receiver)
+std::deque<Message> MessagePool::ReadMessages(const User& receiver)
 {
 	std::deque<Message> usersMessages = std::move(messages[receiver.ID]);
 	messages[receiver.ID].clear();
--- src/Models/MessagePool.h
+++ src/Models/MessagePool.h
@@ -12,7 +12,7 @@ class MessagePool {
 public:
     MessagePool();
     void StoreMessage(Message message);
-    std::deque<Message> ReadMessages(User receiver);
+    std::deque<Message> ReadMessages(const User& receiver);
 private:
     //TODO: make User hashable (same as with the User pool)
     std::unordered_map<std::string, std::deque<Message>> messages;
