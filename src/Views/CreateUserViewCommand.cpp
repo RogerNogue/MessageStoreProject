58a5e9213056995a7201838122db49af95f9b070
RogerNogue <rogernogueb@gmail.com>
2026-06-17 10:20:30 +0200

    refactor: rename variable

--- src/Views/CreateUserViewCommand.cpp
+++ src/Views/CreateUserViewCommand.cpp
@@ -12,18 +12,18 @@ namespace Views {
 	void CreateUserViewCommand::Run() const
 	{
 		cout << "Please enter name: ";
-		string str;
-		getline(cin, str);
+		string userId;
+		getline(cin, userId);
 		cout << endl;
 		//TODO: consider creating a result pattern to only do 1 call to the use case.
-		if (createUser.DoesUserExist(str))
+		if (createUser.DoesUserExist(userId))
 		{
 			cout << "ERROR: User already exists!" << endl;
 		}
 		else
 		{
-			createUser.Run(str);
-			cout << "User " << str << " added!" << endl;
+			createUser.Run(userId);
+			cout << "User " << userId << " added!" << endl;
 		}
 	}
 }// namespace Views
